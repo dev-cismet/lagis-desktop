8caf3db03e8edbd6d563df62f69d849985a46700
Pavel <p-olhovoi@bk.ru>
2023-07-17 15:57:43 +0200

    issue #001 add sizes to offices roles

--- src/stories/overview/OfficesRoles.stories.js
+++ src/stories/overview/OfficesRoles.stories.js
@@ -1,6 +1,7 @@
 import { withDesign } from "storybook-addon-designs";
 import Offices from "../../components/overview/Offices";
 import design from "../assets/overview/Offices.png";
+import { generateStory } from "../_tools/StoryFactory";
 
 export default {
   title: "Overview/Components/OfficesRoles",
@@ -11,6 +12,7 @@ export default {
 export const Mockup = {
   args: {},
 };
+
 Mockup.parameters = {
   design: {
     type: "image",
@@ -18,3 +20,10 @@ Mockup.parameters = {
     scale: 0.5,
   },
 };
+
+export const M = generateStory([340, 218]);
+export const L = generateStory([220, 234]);
+export const XL = generateStory([566, 323]);
+export const S = generateStory([250, 220]);
+export const TabletLandscape = generateStory([200, 190]);
+export const TabletPortrait = generateStory([200, 210]);
