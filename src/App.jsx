aa05a61e0d0e124584b8dcf44fa110c4960af066
Thorsten Hell <thorsten.hell@cismet.de>
2023-07-20 11:32:51 +0200

    wo #3: add first api interactions examples

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,28 @@ import "./App.css";
 import React from "react";
 import { Button, Space } from "antd";
 import { Typography } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getJWT,
+  setLoginRequested,
+  storeJWT,
+  storeLogin,
+} from "./store/slices/auth";
+import { DOMAIN, REST_SERVICE } from "./constants/lagis";
+import queries from "./core/queries/online";
+import { fetchGraphQL } from "./core/graphql";
+import useDevSecrets from "./core/hooks/useDevSecrets";
+import TextArea from "antd/es/input/TextArea";
+
 const { Title } = Typography;
 function App() {
   const [count, setCount] = useState(0);
+  const jwt = useSelector(getJWT);
+  const dispatch = useDispatch();
+  const { user, pw } = useDevSecrets();
+  const [text, setText] = useState("");
+
+  console.log("jwt", jwt);
 
   return (
     <div className="App">
@@ -39,8 +58,103 @@ function App() {
         <Button type="text">Text Button</Button>
         <Button type="link">Link Button</Button>
       </Space>
+      <div style={{ marginTop: 100 }}>
+        <div>{jwt !== undefined ? "Logged in" : "not logged in"}</div>
+        <Space>
+          <Button
+            onClick={() => {
+              login(user, pw, dispatch);
+            }}
+          >
+            Login
+          </Button>
+          <Button
+            onClick={() => {
+              setText(jwt);
+            }}
+          >
+            JWT
+          </Button>
+          <Button
+            onClick={() => {
+              const gqlQuery = queries.first;
+
+              const queryParameter = {
+                gemarkung: "Barmen",
+                flur: 1,
+                fstkZaehler: 367,
+                fstkNenner: 0,
+              };
+
+              //local async query
+              (async () => {
+                const result = await fetchGraphQL(
+                  gqlQuery,
+                  queryParameter,
+                  jwt
+                );
+                console.log("result", result);
+                setText(JSON.stringify(result, null, 2));
+              })();
+            }}
+          >
+            Query
+          </Button>
+          <Button
+            onClick={() => {
+              //local async query
+              (async () => {
+                const result = await fetchGraphQL(queries.keys, {}, jwt);
+                setText(JSON.stringify(result, null, 2));
+                console.log("result", result);
+              })();
+            }}
+          >
+            Keys
+          </Button>
+        </Space>
+      </div>
+
+      <div style={{ marginTop: 100 }}>
+        <TextArea
+          value={text}
+          autoSize={{
+            minRows: 2,
+            maxRows: 20,
+          }}
+        ></TextArea>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const login = (user, pw, dispatch) => {
+  fetch(REST_SERVICE + "/users", {
+    method: "GET",
+    headers: {
+      Authorization: "Basic " + btoa(user + "@" + DOMAIN + ":" + pw),
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      if (response.status >= 200 && response.status < 300) {
+        response.json().then(function (responseWithJWT) {
+          const jwt = responseWithJWT.jwt;
+          console.log("Anmeldung erfolgreich.");
+
+          setTimeout(() => {
+            dispatch(storeJWT(jwt));
+            dispatch(storeLogin(user));
+            dispatch(setLoginRequested(false));
+          }, 500);
+        });
+      } else {
+        console.log("Bei der Anmeldung ist ein Fehler aufgetreten. ");
+      }
+    })
+    .catch(function (err) {
+      console.log("Bei der Anmeldung ist ein Fehler aufgetreten. ", err);
+    });
+};
