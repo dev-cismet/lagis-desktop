6502db39ff85e31e4664a0ea881f37d2e8435d67
Pavel <p-olhovoi@bk.ru>
2023-07-26 18:42:27 +0200

    issue #001 change the grid on the Rights page

--- src/pages/RightsPage.jsx
+++ src/pages/RightsPage.jsx
@@ -6,6 +6,7 @@ import Agencies from "../components/offices/Agencies";
 import AdditionalRole from "../components/offices/AdditionalRole";
 import Streetfronts from "../components/offices/Streetfronts";
 import Notes from "../components/offices/Notes";
+import RightsAndEncumbrances from "../components/rights/RightsAndEncumbrances";
 
 const RightsPage = ({ width = "100%", height = "100%", inStory = false }) => {
   let storyStyle = {};
@@ -16,21 +17,22 @@ const RightsPage = ({ width = "100%", height = "100%", inStory = false }) => {
       padding: "10px",
     };
   }
-  const firstRowStyle = { width: "100%", height: height * 0.6 - 12 };
-  const secondRowStyle = { width: "100%", height: height * 0.4 - 12 };
+  const firstRowStyle = { width: "100%", height: height * 0.5 - 12 };
+  const secondRowStyle = { width: "100%", height: height * 0.5 - 12 };
   const gutterStyle = [12, 12];
   const marginBottomStyle = { marginBottom: "12px" };
   return (
     <div style={{ ...storyStyle, width, height }}>
-      <Row gutter={gutterStyle} style={{ height: "60%", ...marginBottomStyle }}>
+      <Row gutter={gutterStyle} style={{ height: "50%", ...marginBottomStyle }}>
         <Col span={24}>
           <Map width={"100%"} height={firstRowStyle.height} />
         </Col>
       </Row>
 
-      <Row gutter={gutterStyle} style={{ height: "40%" }}>
+      <Row gutter={gutterStyle} style={{ height: "50%" }}>
         <Col span={24}>
-          <MockCard style={secondRowStyle} title="Additional Role" />
+          {/* <MockCard style={secondRowStyle} title="Additional Role" /> */}
+          <RightsAndEncumbrances />
         </Col>
       </Row>
     </div>
