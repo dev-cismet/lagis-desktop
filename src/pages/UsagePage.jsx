dd1767409ccac03f9038ea17b5a6c6aaef553da4
Pavel <p-olhovoi@bk.ru>
2023-07-28 08:25:07 +0200

    issue #001 add the usage page with the story

---
+++ src/pages/UsagePage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Map from "../components/commons/Map";
+import { Col, Row } from "antd";
+import MockCard from "../components/mock/MockCard";
+
+const UsagePage = ({ width = "100%", height = "100%", inStory = false }) => {
+  let storyStyle = {};
+  if (inStory) {
+    storyStyle = {
+      borderStyle: "dotted",
+      borderWidth: "1px solid",
+      padding: "4px",
+    };
+  }
+  const firstRowStyle = { width: "100%", height: height * 0.4 - 12 };
+  const gutterStyle = [0, 0];
+  const marginBottomStyle = { marginBottom: "0px" };
+  return (
+    <div
+      style={{
+        ...storyStyle,
+        width,
+        height,
+        backgroundColor: "#F1F1F1",
+      }}
+    >
+      <Row gutter={gutterStyle} style={{ height: "40%", ...marginBottomStyle }}>
+        <Col span={24}>
+          <Map width={"100%"} height={firstRowStyle.height} />
+        </Col>
+      </Row>
+      <Row gutter={gutterStyle} style={{ height: "60%" }}>
+        <Col span={24}>
+          <MockCard />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default UsagePage;
