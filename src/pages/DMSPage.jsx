05575588805cd6456b906eac712306fdcbe778d9
Pavel <p-olhovoi@bk.ru>
2023-08-02 11:31:42 +0200

    issue #001 add the DMS page with the storybook

---
+++ src/pages/DMSPage.jsx
@@ -0,0 +1,29 @@
+import { Col, Row } from "antd";
+const DMSPage = ({ width = "100%", height = "100%", inStory = false }) => {
+  let storyStyle = {};
+  if (inStory) {
+    storyStyle = {
+      borderStyle: "dotted",
+      borderWidth: "1px solid",
+      background: "gray",
+      padding: "4px",
+    };
+  }
+  const gutterStyle = [16, 16];
+  return (
+    <div
+      style={{
+        ...storyStyle,
+        width,
+        height,
+        backgroundColor: "#F1F1F1",
+      }}
+    >
+      <Row gutter={gutterStyle}>
+        <Col span={24}>DMS</Col>
+      </Row>
+    </div>
+  );
+};
+
+export default DMSPage;
