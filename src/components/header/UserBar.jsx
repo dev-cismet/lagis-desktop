e2a0bf5af6883a8dc4f3d8b82bf5febbc182e730
Pavel <p-olhovoi@bk.ru>
2023-08-02 19:09:23 +0200

    issue #001 add selectors to the user bar

---
+++ src/components/header/UserBar.jsx
@@ -0,0 +1,10 @@
+import HeaderSelectors from "../ui/filters/HeaderSelectors";
+
+const UserBar = () => {
+  return (
+    <div>
+      <HeaderSelectors />
+    </div>
+  );
+};
+export default UserBar;
---
+++ src/components/ui/filters/HeaderSelectors.jsx
@@ -0,0 +1,40 @@
+import { Select, Space } from "antd";
+const HeaderSelectors = () => {
+  const names = [
+    { value: "Vohwinkel", label: "vohwinkel" },
+    { value: "VohwinkelOne", label: "vohwinkelone" },
+  ];
+  const numbersSmall = [
+    { value: 20, label: "20" },
+    { value: 10, label: "10" },
+  ];
+  const numbersBig = [
+    { value: 120, label: "120" },
+    { value: 210, label: "210" },
+  ];
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  return (
+    <div>
+      <Space>
+        <Select
+          defaultValue="Vohwinkel"
+          onChange={handleChange}
+          options={names}
+        />
+        <Select
+          defaultValue="10"
+          onChange={handleChange}
+          options={numbersSmall}
+        />
+        <Select
+          defaultValue="10"
+          onChange={handleChange}
+          options={numbersBig}
+        />
+      </Space>
+    </div>
+  );
+};
+export default HeaderSelectors;
