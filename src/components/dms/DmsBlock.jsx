ca02fd272753ed3961bc96fbd976d4b6d707bebc
Pavel <p-olhovoi@bk.ru>
2023-08-02 12:29:42 +0200

    issue #001 add content to the DMS block

---
+++ src/components/dms/DmsBlock.jsx
@@ -0,0 +1,137 @@
+import PropTypes from "prop-types";
+import InfoBlock from "../ui/Blocks/InfoBlock";
+import ToggleModal from "../ui/control-board/ToggleModal";
+import TableMock from "../ui/tables/TableMock";
+import ModalForm from "../ui/forms/ModalForm";
+import mockFoto from "../../assets/docksMock.png";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Dateiname",
+    dataIndex: "file",
+    key: "file",
+  },
+  {
+    title: "Beschreibung",
+    dataIndex: "beschreibung",
+    key: "beschreibung",
+  },
+  {
+    title: "Vorschau",
+    dataIndex: "vorschau",
+    key: "vorschau",
+    render: (image) => (
+      <img src={image} alt="User" style={{ width: 80, height: 70 }} />
+    ),
+  },
+];
+const mockExtractor = (input) => {
+  return [
+    {
+      key: "1",
+      name: "1237563255",
+      file: "Interdum.avi",
+      beschreibung: "Lorem ipsum dolor sit amet",
+      vorschau: mockFoto,
+    },
+    {
+      key: "2",
+      name: "1237563255",
+      file: "Interdum.avi",
+      beschreibung: "Lorem ipsum dolor sit amet",
+      vorschau: mockFoto,
+    },
+    {
+      key: "3",
+      name: "1237563255",
+      file: "Interdum.avi",
+      beschreibung: "Lorem ipsum dolor sit amet",
+      vorschau: mockFoto,
+    },
+    {
+      key: "4",
+      name: "1237563255",
+      file: "Interdum.avi",
+      beschreibung: "Lorem ipsum dolor sit amet",
+      vorschau: mockFoto,
+    },
+  ];
+};
+const DmsBlock = ({
+  dataIn,
+  extractor = mockExtractor,
+  width = 231,
+  height = 188,
+  style,
+}) => {
+  const data = extractor(dataIn);
+  const isStory = false;
+  const storyStyle = { width, height, ...style };
+  return (
+    <div
+      style={
+        isStory
+          ? storyStyle
+          : { height: "100%", borderRadius: "6px", backgroundColor: "#FFFFFF" }
+      }
+    >
+      <InfoBlock
+        title="Dokumenten Management System"
+        controlBar={
+          <ToggleModal
+            section="Kassenzeichen"
+            content={
+              <ModalForm
+                fields={[
+                  { title: "Name", rules: [{ required: true }] },
+                  { title: "Dateiname", rules: [{ required: true }] },
+                  { title: "Beschreibung", rules: [{ required: true }] },
+                ]}
+              />
+            }
+          />
+        }
+      >
+        <TableMock columns={columns} data={data} />
+      </InfoBlock>
+    </div>
+  );
+};
+
+export default DmsBlock;
+DmsBlock.propTypes = {
+  /**
+   * The current main data object that is being used
+   */
+  dataIn: PropTypes.array,
+  /**
+   * The extractor function that is used to transform the dataIn object into the data object
+   */
+  extractor: PropTypes.func,
+  /**
+   * The width of the component
+   * @default 300
+   * @type number
+   * @required false
+   * @control input
+   * @group size
+   *
+   **/
+  width: PropTypes.number,
+
+  /**
+   * The height of the component
+   *
+   * @default 300
+   * @type number
+   * @required false
+   * @control input
+   *
+   **/
+
+  height: PropTypes.number,
+};
