04793249deac5c6e55e32a184ad1d2ef4ed33dbf
Pavel <p-olhovoi@bk.ru>
2023-08-07 11:31:43 +0200

    issue #001 fix the adaptive behavior of the popup toggle bar

--- src/components/ui/heads/HeadBlock.jsx
+++ src/components/ui/heads/HeadBlock.jsx
@@ -2,7 +2,7 @@ import { MenuOutlined } from "@ant-design/icons";
 const HeadBlock = ({ title, children, titleAction }) => {
   return (
     <div
-      className="flex justify-start items-center px-3"
+      className="flex flex-wrap justify-between items-center px-3"
       style={{
         borderBottom: "1px solid #C4C4C4",
         backgroundColor: "#FFFFFF",
@@ -11,13 +11,13 @@ const HeadBlock = ({ title, children, titleAction }) => {
     >
       <MenuOutlined style={{ fontSize: "14px" }} />
       <h3
-        className="mx-0 my-3 text-left ml-3 mr-3 text-base"
+        className="mr-auto my-3 text-left ml-3 mr-3 text-base"
         style={{ color: "#474747" }}
       >
         {title}
       </h3>
       {titleAction && titleAction}
-      <div className="ml-auto"> {children}</div>
+      <div className="my-1"> {children}</div>
     </div>
   );
 };
