b01e24996d72206522a9a09da99d179793fd5322
Pavel <p-olhovoi@bk.ru>
2023-08-08 08:35:53 +0200

    issue #001 add routes for all pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,64 @@ import "antd/dist/reset.css";
 import { Provider } from "react-redux";
 import store from "./store";
 import ErrorPage from "./components/ui/errors-template/ErrorsPage";
+import Overview from "./pages/Overview";
+import AppLayout from "./pages/AppLayout";
+import Offices from "./pages/Offices";
+import RentAndLease from "./pages/RentAndLease";
+import RightsPage from "./pages/RightsPage";
+import UsagePage from "./pages/UsagePage";
+import OperationsPage from "./pages/OperationsPage";
+import HistoryPage from "./pages/HistoryPage";
+import Transaction from "./pages/Transaction";
+import DMSPage from "./pages/DMSPage";
+import LoginPage from "./components/login/LoginPage";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <AppLayout />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Overview />,
+      },
+      {
+        path: "/verwaltungsbereiche",
+        element: <Offices />,
+      },
+      {
+        path: "/miet",
+        element: <RentAndLease />,
+      },
+      {
+        path: "/rechte",
+        element: <RightsPage />,
+      },
+      {
+        path: "/nutzung",
+        element: <UsagePage />,
+      },
+      {
+        path: "/operations",
+        element: <OperationsPage />,
+      },
+      {
+        path: "/historie",
+        element: <HistoryPage />,
+      },
+      {
+        path: "/transaction",
+        element: <Transaction />,
+      },
+      {
+        path: "/documents",
+        element: <DMSPage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Row, Col } from "antd";
+import SidebarMenu from "../components/navigation/SidebarMenu";
+import UserBar from "../components/header/UserBar";
+import Overview from "./Overview";
+import FooterSection from "../components/navigation/FooterSection";
+import { Outlet } from "react-router-dom";
+const AppLayout = ({ width = "100%", height = "100%", inStory = false }) => {
+  let storyStyle = {};
+  if (inStory) {
+    storyStyle = {
+      borderStyle: "dotted",
+      borderWidth: "1px",
+      borderColor: "#ddd",
+    };
+  }
+
+  return (
+    <div
+      style={{
+        ...storyStyle,
+        width,
+        height,
+        display: "flex",
+        flexDirection: "column",
+        background: "#F1F1F1",
+      }}
+      className="pr-4"
+    >
+      <Row
+        style={{
+          height: "100%",
+        }}
+        gutter={[16, 16]}
+      >
+        <Col span={5}>
+          <SidebarMenu />
+        </Col>
+        <Col span={19} style={{ display: "flex", flexDirection: "column" }}>
+          <Row>
+            <Col span={24}>
+              <UserBar />
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: "32px" }}>
+            <Col span={24}></Col>
+          </Row>
+          <Row style={{ marginBottom: "36px" }}>
+            <Col span={24}>
+              <Outlet />
+            </Col>
+          </Row>
+          <Row style={{ marginTop: "auto" }}>
+            <Col span={24}>
+              <FooterSection />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default AppLayout;
