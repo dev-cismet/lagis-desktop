936f4c99480bfa6a54113dd2d934175fbd5316f3
Pavel <p-olhovoi@bk.ru>
2023-08-22 15:18:58 +0200

    issue #019 collapse menu on screens smaller than 768

--- src/components/navigation/SidebarMenu.jsx
+++ src/components/navigation/SidebarMenu.jsx
@@ -14,6 +14,7 @@ import { useState } from "react";
 import { Menu, Button } from "antd";
 import "./menu.css";
 import Logo from "../ui/logo/Logo";
+import { useEffect } from "react";
 function getItem(label, key, icon, children) {
   return {
     key,
@@ -44,7 +45,9 @@ const SidebarMenu = ({ activeKey = ["1"] }) => {
     width: isStory ? storyWidth : "100%",
     height: isStory ? "600px" : "100%",
   };
-
+  useEffect(() => {
+    window.innerWidth <= 768 && setCollapsed(true);
+  }, []);
   return (
     <div
       className="flex flex-col bg-white max-w-min"
@@ -66,7 +69,6 @@ const SidebarMenu = ({ activeKey = ["1"] }) => {
 
       <Menu
         style={{
-          // minWidth: "10px",
           border: 0,
         }}
         defaultSelectedKeys={activeKey}
