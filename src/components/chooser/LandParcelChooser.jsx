700c849e358bcd7e60fa4df1ddd7b218904e933f
Pavel <p-olhovoi@bk.ru>
2023-09-04 08:43:08 +0200

    issue #001 fix sorting method for Flurstuecke select

--- src/components/chooser/LandParcelChooser.jsx
+++ src/components/chooser/LandParcelChooser.jsx
@@ -92,7 +92,6 @@ const LandParcelChooser = ({
       flurstueckRef.current.focus();
     }, 10);
   };
-  // flurstueckRef.current.focus()
   const handleFlurstueckChange = (flurstueckValue) => {
     flurstueckChoosen({
       gemarkung: selectedGemarkung.gemarkung,
@@ -112,11 +111,7 @@ const LandParcelChooser = ({
       flurstueckRef.current.focus();
     }
   };
-  // const handleKeyFlurstueck = (e) => {
-  //   if (e.key === "Enter") {
-  //     flurstueckRef.current.focus();
-  //   }
-  // };
+
   return (
     <>
       <Select
@@ -124,7 +119,7 @@ const LandParcelChooser = ({
         showSearch
         placeholder="Gemarkung"
         style={{
-          width: 200,
+          width: 160,
         }}
         filterOption={(input, option) =>
           (option?.label ?? "").toLowerCase().startsWith(input)
@@ -148,7 +143,7 @@ const LandParcelChooser = ({
         key={"Flure.for." + (selectedGemarkung?.gemarkung || "-")}
         showSearch
         style={{
-          width: 200,
+          width: 100,
         }}
         onKeyDown={handleKeyFlur}
         className="mx-1"
@@ -175,45 +170,13 @@ const LandParcelChooser = ({
         placeholder="Flurstück"
         showSearch
         style={{
-          width: 200,
+          width: 100,
         }}
         filterOption={(input, option) =>
           (option?.value ?? "").toLowerCase().startsWith(input)
         }
         filterSort={(optionA, optionB) => {
-          const compareNumericParts = (a, b) => {
-            const numericA = parseFloat(a) || 0;
-            const numericB = parseFloat(b) || 0;
-            return numericA - numericB;
-          };
-
-          const labelA = optionA.value;
-          const labelB = optionB.value;
-
-          const partsA = labelA.match(/\d+|\D+/g);
-          const partsB = labelB.match(/\d+|\D+/g);
-
-          for (let i = 0; i < Math.min(partsA.length, partsB.length); i++) {
-            const partA = partsA[i];
-            const partB = partsB[i];
-
-            if (!isNaN(partA) && !isNaN(partB)) {
-              const result = compareNumericParts(partA, partB);
-              if (result !== 0) {
-                return result;
-              }
-            }
-
-            if (isNaN(partA) || isNaN(partB)) {
-              const result = partA.localeCompare(partB);
-              if (result !== 0) {
-                return result;
-              }
-            }
-          }
-
-          // If all parts are the same up to this point, compare the lengths
-          return partsA.length - partsB.length;
+          return parseFloat(optionA.value) - parseFloat(optionB.value);
         }}
         onChange={handleFlurstueckChange}
         options={Object.keys(selectedFlur?.flurstuecke || []).map((key) => {
@@ -263,24 +226,4 @@ const removeLeadingZeros = (numberStr) => {
   const result = trimmedParts.join("/");
 
   return result;
-};
-
-const numericSort = (a, b) => {
-  // Split values into numeric and non-numeric parts
-  const [aNumeric, aNonNumeric] = a.match(/\d+|\D+/g);
-  const [bNumeric, bNonNumeric] = b.match(/\d+|\D+/g);
-
-  // Convert numeric parts to numbers
-  const numericA = parseFloat(aNumeric) || 0;
-  const numericB = parseFloat(bNumeric) || 0;
-
-  // Compare numeric parts
-  if (numericA < numericB) {
-    return -1;
-  } else if (numericA > numericB) {
-    return 1;
-  } else {
-    // If numeric parts are equal, compare non-numeric parts
-    return aNonNumeric.localeCompare(bNonNumeric);
-  }
 };
