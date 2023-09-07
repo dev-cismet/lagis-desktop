88d90a36da7f382b8684be7ef6aa4c352e8c8542
Thorsten Hell <thorsten.hell@cismet.de>
2023-09-07 19:07:12 +0200

    adding basic undefined fallback behaviour for rentExtractor

--- src/core/extractors/overviewExtractors.js
+++ src/core/extractors/overviewExtractors.js
@@ -1,8 +1,15 @@
 export function rentExtractor(dataIn) {
-  const landparcel = dataIn[0];
-  const numberOfRents = landparcel?.ar_vertraegeArray?.length || 0;
-  return {
-    numberOfRents,
-    color: numberOfRents > 0 ? "#5D5FEF" : "#999999",
-  };
+  if (dataIn === undefined) {
+    return {
+      numberOfRents: "  ",
+      color: "#BBBBBB",
+    };
+  } else {
+    const landparcel = dataIn[0];
+    const numberOfRents = landparcel?.ar_vertraegeArray?.length || 0;
+    return {
+      numberOfRents,
+      color: numberOfRents > 0 ? "#5D5FEF" : "#999999",
+    };
+  }
 }
