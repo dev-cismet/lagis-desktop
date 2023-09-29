5ea8958b6dccafdc48ed5ad14d190eb6cd025f86
Pavel <p-olhovoi@bk.ru>
2023-09-29 14:27:57 +0200

    issue #041 fix dms links

--- src/core/extractors/dmsPageExtractor.js
+++ src/core/extractors/dmsPageExtractor.js
@@ -5,18 +5,29 @@ export function dmsPageExtractor(dataIn) {
     const dms = dataIn?.dms_urlArrayRelationShip || [];
     if (dms.length > 0) {
       const data = dms.map((d) => {
-        //href="//sw0040/Lagerbuch/Inhaltsverzeichnis_LB/Barmen/002.TIF"
-        const stringWithForwardSlashes = d.url.url_base.path
-          .replace(/\\/g, "/")
-          .replace(/\s/g, "_");
-        console.log("dms extractor", d);
-        const server = d.url.url_base.server;
+        const server = "http://dokumente.s10222.wuppertal-intra.de";
+        const fileName = d.url.object_name;
+        const urlBase = d.url.url_base.path;
+        const pathParts = urlBase.split("\\");
+        pathParts[1] = pathParts[1].toLowerCase();
+        const modifiedPath = pathParts.join("/");
+        const finalPath = `${server}${modifiedPath}${fileName.replace(
+          /\s/g,
+          "%20"
+        )}`;
+
+        console.log("dms link urlData", finalPath);
+
+        // const stringWithForwardSlashes = d.url.url_base.path
+        //   .replace(/\\/g, "/")
+        //   .replace(/\s/g, "%20");
+
         return {
           id: d.id,
           name: d.name,
           beschreibung: d.beschreibung === null ? "" : d.beschreibung,
           file: d.url.object_name,
-          vorschau: `//${server}${stringWithForwardSlashes}${d.url.object_name}`,
+          vorschau: finalPath,
         };
       });
 
