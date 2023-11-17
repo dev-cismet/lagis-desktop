9cb7362aa4bf42f577ccbda299542ae07ab164bd
Pavel <p-olhovoi@bk.ru>
2023-11-17 11:09:50 +0100

    issue #100 refactor get and save of selected landparcel

--- src/components/header/UserBar.jsx
+++ src/components/header/UserBar.jsx
@@ -20,6 +20,7 @@ import {
   storeMipa,
   storeHistory,
   storeGeometry,
+  getTestFlurstueck,
 } from "../../store/slices/lagis";
 import { getSyncLandparcel } from "../../store/slices/ui";
 import { useSelector, useDispatch } from "react-redux";
@@ -38,102 +39,98 @@ const UserBar = () => {
   const { landParcels } = useSelector(getLandParcels);
   const { landmarks } = useSelector(getLandmarks);
   const getFlurstueck = async (schluessel_id, alkis_id) => {
-    const result = await fetchGraphQL(
-      queries.getLagisLandparcelByFlurstueckSchluesselId,
-      {
-        schluessel_id,
-        alkis_id,
-      },
-      jwt
-    );
-    if (result.status === 401) {
-      return navigate("/login");
-    }
-    const f = result?.data.flurstueck[0];
-    f.alkisLandparcel = result?.data.extended_alkis_flurstueck[0];
-    dispatch(storeLagisLandparcel(f));
-    dispatch(storeAlkisLandparcel(f.alkisLandparcel));
-
-    let geo =
-      result?.data.flurstueck[0].extended_geom?.geo_field ||
-      result?.data.flurstueck[0].alkisLandparcel?.geometrie;
-
-    if (!geo) {
-      const result = await getGeomFromWuNDa(alkis_id);
-      geo = result.data.flurstueck[0].extended_geom.geo_field;
-      // console.log("xxx geo was not set. is now:", geo);
-    }
-    dispatch(storeGeometry(geo));
-
-    if (geo) {
-      await getRebe(getBuffer25832(geo, -1));
-      await getMipa(getBuffer25832(geo, -1));
-    } else {
-      dispatch(storeRebe());
-      dispatch(storeMipa());
-    }
-
-    await getHistory(schluessel_id);
+    // const result = await fetchGraphQL(
+    //   queries.getLagisLandparcelByFlurstueckSchluesselId,
+    //   {
+    //     schluessel_id,
+    //     alkis_id,
+    //   },
+    //   jwt
+    // );
+    // if (result.status === 401) {
+    //   return navigate("/login");
+    // }
+    // const f = result?.data.flurstueck[0];
+    // f.alkisLandparcel = result?.data.extended_alkis_flurstueck[0];
+    // dispatch(storeLagisLandparcel(f));
+    // dispatch(storeAlkisLandparcel(f.alkisLandparcel));
+    // let geo =
+    //   result?.data.flurstueck[0].extended_geom?.geo_field ||
+    //   result?.data.flurstueck[0].alkisLandparcel?.geometrie;
+    // if (!geo) {
+    //   const result = await getGeomFromWuNDa(alkis_id);
+    //   geo = result.data.flurstueck[0].extended_geom.geo_field;
+    //   // console.log("xxx geo was not set. is now:", geo);
+    // }
+    // dispatch(storeGeometry(geo));
+    // if (geo) {
+    //   await getRebe(getBuffer25832(geo, -1));
+    //   await getMipa(getBuffer25832(geo, -1));
+    // } else {
+    //   dispatch(storeRebe());
+    //   dispatch(storeMipa());
+    // }
+    // await getHistory(schluessel_id);
   };
-  const getRebe = async (geo) => {
-    const result = await fetchGraphQL(
-      queries.getRebeByGeo,
-      {
-        geo,
-      },
-      jwt
-    );
-    if (result.status === 401) {
-      return navigate("/login");
-    }
-    dispatch(storeRebe(result?.data?.rebe));
-  };
-  const getMipa = async (geo) => {
-    const result = await fetchGraphQL(
-      queries.getMipaByGeo,
-      {
-        geo,
-      },
-      jwt
-    );
-    if (result.status === 401) {
-      return navigate("/login");
-    }
-    dispatch(storeMipa(result?.data?.mipa));
-  };
+  // const getRebe = async (geo) => {
+  //   const result = await fetchGraphQL(
+  //     queries.getRebeByGeo,
+  //     {
+  //       geo,
+  //     },
+  //     jwt
+  //   );
+  //   if (result.status === 401) {
+  //     return navigate("/login");
+  //   }
+  //   dispatch(storeRebe(result?.data?.rebe));
+  // };
+  // const getMipa = async (geo) => {
+  //   const result = await fetchGraphQL(
+  //     queries.getMipaByGeo,
+  //     {
+  //       geo,
+  //     },
+  //     jwt
+  //   );
+  //   if (result.status === 401) {
+  //     return navigate("/login");
+  //   }
+  //   dispatch(storeMipa(result?.data?.mipa));
+  // };
 
-  const getGeomFromWuNDa = async (alkis_id) => {
-    const result = await fetchGraphQLFromWuNDa(
-      queries.getGeomFromWuNDA,
-      {
-        alkis_id,
-      },
-      jwt
-    );
-    if (result.status === 401) {
-      return navigate("/login");
-    }
-    return result;
-  };
+  // const getGeomFromWuNDa = async (alkis_id) => {
+  //   const result = await fetchGraphQLFromWuNDa(
+  //     queries.getGeomFromWuNDA,
+  //     {
+  //       alkis_id,
+  //     },
+  //     jwt
+  //   );
+  //   if (result.status === 401) {
+  //     return navigate("/login");
+  //   }
+  //   return result;
+  // };
 
-  const getHistory = async (sid) => {
-    // console.log("xxx queries.getHistory", sid);
-    try {
-      const result = await fetchGraphQL(
-        queries.history,
-        {
-          schluessel_id: sid,
-        },
-        jwt
-      );
-      if (result.status === 401) {
-        return navigate("/login");
-      }
-      dispatch(storeHistory(result?.data?.cs_calc_history));
-    } catch (e) {
-      console.log("xxx error in getHistory", e);
-    }
-  };
+  // const getHistory = async (sid) => {
+  //   // console.log("xxx queries.getHistory", sid);
+  //   try {
+  //     const result = await fetchGraphQL(
+  //       queries.history,
+  //       {
+  //         schluessel_id: sid,
+  //       },
+  //       jwt
+  //     );
+  //     if (result.status === 401) {
+  //       return navigate("/login");
+  //     }
+  //     dispatch(storeHistory(result?.data?.cs_calc_history));
+  //   } catch (e) {
+  //     console.log("xxx error in getHistory", e);
+  //   }
+  // };
   const handleOpenLandparcelInJavaApp = (fstck) => {
     if (syncLandparcel) {
       const gemarkung = fstck.gemarkung;
@@ -156,7 +153,8 @@ const UserBar = () => {
         flurstueckChoosen={(fstck) => {
           // console.log("flurstueckChoosen", fstck);
           if (fstck.lfk) {
-            getFlurstueck(fstck.lfk, fstck.alkis_id);
+            // getFlurstueck(fstck.lfk, fstck.alkis_id);
+            dispatch(getTestFlurstueck(fstck.lfk, fstck.alkis_id));
             handleOpenLandparcelInJavaApp(fstck);
           }
         }}
