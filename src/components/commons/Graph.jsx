ffca338087a7f691d1f81e0ac0317b42517275c5
Pavel <p-olhovoi@bk.ru>
2023-11-21 20:28:08 +0100

    issue #100 fix prop type for history and  transaction pages

--- src/components/commons/Graph.jsx
+++ src/components/commons/Graph.jsx
@@ -218,7 +218,7 @@ Graph.propTypes = {
   /**
    * The current main data object that is being used
    */
-  dataIn: PropTypes.object,
+  dataIn: PropTypes.array,
   /**
    * The extractor function that is used to transform the dataIn object into the data object
    */
