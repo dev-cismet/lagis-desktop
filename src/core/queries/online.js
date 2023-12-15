78c40addefabfbd67f132870804a523d06acdab7
Thorsten Hell <thorsten.hell@cismet.de>
2023-12-15 20:59:12 +0100

    don't load historical landparcels

--- src/core/queries/online.js
+++ src/core/queries/online.js
@@ -496,9 +496,11 @@ queries.getFlurstuckelByMipaFileNumber = `query MyQuery($aktz: String) {
 }`;
 
 queries.getFstckForPoint = `query q($x: Float!, $y: Float!) {
-  flurstueck(where: {geom: {geo_field: {_st_intersects: {type: "Point", crs: {type: "name", properties: {name: "urn:ogc:def:crs:EPSG::25832"}}, coordinates: [$x, $y]}}}}) {
+  flurstueck(where: {geom: {geo_field: {_st_intersects: {type: "Point", crs: {type: "name", properties: {name: "urn:ogc:def:crs:EPSG::25832"}}, coordinates: [$x, $y]}}}, historisch: {_is_null: true}}) {
     alkis_id
-    geom {
+    historisch
+    extended_geom {
+      area
       geo_field
     }
   }
